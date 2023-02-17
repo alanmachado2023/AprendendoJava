1f07a53c703aa728efd784bde1889445fc0c24fc
Alan Machado <dev.alan.machado@gmail.com>
2023-02-17 19:02:36 -0300

    Aulas de criação de objetos com uso de getters e setters

--- AprendendoJava/src/aprendendoACriar/ImplementandoClasse.java
+++ AprendendoJava/src/aprendendoACriar/ImplementandoClasse.java
@@ -1,5 +1,7 @@
 package aprendendoACriar;
 
+import javax.swing.JOptionPane;
+
 import aprendendoClassses.Aluno;
 
 public class ImplementandoClasse {
@@ -7,11 +9,68 @@ public class ImplementandoClasse {
 	//Main é um método auto executável em Java
 	public static void main(String[] args) {
 		
-		Aluno aluno1 = new Aluno(); //aqui será o João
+		String nome = JOptionPane.showInputDialog("Qual o nome do aluno? ");
+		String idade = JOptionPane.showInputDialog("Qual a idade? ");
+		String dataNascimento = JOptionPane.showInputDialog("Qual a data de nascimento ");
+		String registroGeral = JOptionPane.showInputDialog("Informe o RG ");
+		String numeroCpf = JOptionPane.showInputDialog("Informe o CPF ");
+		String nomeMae = JOptionPane.showInputDialog("Qual o nome da mãe? ");
+		String nomePai = JOptionPane.showInputDialog("Qual o nome do pai? ");
+		String dataMatricula = JOptionPane.showInputDialog("Qual a data de matrícula? ");
+		String serieMatricula = JOptionPane.showInputDialog("Qual série está matriculado? ");
+		String nomeEscola = JOptionPane.showInputDialog("Qual o nome da escola? ");
+		String nota1 = JOptionPane.showInputDialog("Qual valor da nota 1? ");
+		String nota2 = JOptionPane.showInputDialog("Qual valor da nota 2? ");
+		String nota3 = JOptionPane.showInputDialog("Qual valor da nota 3? ");
+		String nota4 = JOptionPane.showInputDialog("Qual valor da nota 4? ");
 		
-		Aluno aluno2 = new Aluno(); //aqui será o Pedro
 		
-		Aluno aluno3 = new Aluno(); //aqui será o Alex
+		Aluno aluno1 = new Aluno(); //criando o objeto aluno
+		
+		aluno1.setNome(nome);
+		aluno1.setIdade (Integer.valueOf(idade));  //precisa fazer a conversão de string para int usando método "integer"
+		aluno1.setDataNascimento(dataNascimento);
+		aluno1.setRegistroGeral(registroGeral);
+		aluno1.setNumeroCpf(numeroCpf);
+		aluno1.setNomeMae(nomeMae);
+		aluno1.setNomePai(nomePai);
+		aluno1.setDataMatricula("10/01/2019");
+		aluno1.setSerieMatriculado("5ª");
+		aluno1.setNomeEscola("JDEV Treinamento");
+		aluno1.setNota1(Double.parseDouble(nota1));  //Usa o Double.parseDouble para converter para double o valor da string
+		aluno1.setNota2(Double.parseDouble(nota2));
+		aluno1.setNota3(Double.parseDouble(nota3));
+		aluno1.setNota4(Double.parseDouble(nota4));
+		
+		System.out.println("Nome é = " + aluno1.getNome());
+		System.out.println("Idade é = " + aluno1.getIdade());
+		System.out.println("Nascimento é = " + aluno1.getDataNascimento());
+		System.out.println("Média da nota é = " + aluno1.getMediaNota());
+		System.out.println("Resultado = " + aluno1.getAlunoAprovado2());
+		System.out.println();
+		
+		/*=======================================================================*/
+		
+		Aluno aluno2 = new Aluno(); //segundo objeto aluno
+		
+		aluno2.setNome(nome);
+		aluno2.setIdade(49);
+		aluno2.setDataNascimento("12/10/1983");
+		aluno2.setRegistroGeral("561328");
+		aluno2.setNumeroCpf("1579965534");
+		aluno2.setNomeMae("Robiscleia");
+		aluno2.setNomePai("Arlindolfo");
+		aluno2.setDataMatricula("10/05/2018");
+		aluno2.setSerieMatriculado("8ª");
+		aluno2.setNomeEscola("JDEV Treinamento");
+		
+		System.out.println("Nome do aluno 2 é = " + aluno2.getNome());
+		System.out.println("Idade do aluno 2 é = " + aluno2.getIdade());
+		System.out.println("Nascimento do aluno 2 é = " + aluno2.getDataNascimento());
+		
+		/*=======================================================================*/
+		
+		Aluno aluno3 = new Aluno(); 
 		
 		Aluno aluno4 = new Aluno("Maria"); // aqui já inicia com valor padrão usando o Construtor nomePadrão
 
--- AprendendoJava/src/aprendendoClassses/Aluno.java
+++ AprendendoJava/src/aprendendoClassses/Aluno.java
@@ -4,17 +4,22 @@ package aprendendoClassses;
 
 public class Aluno {
 	/*Esses são os atributos do objeto aluno*/
-	String nome;
-	int idade;
-	String dataNascimento;
-	String registroGeral;
-	String numeroCpf;
-	String nomeMae;
-	String nomePai;
-	String dataMatricula;
-	String nomeEscola;
-	String serieMatriculado;
-
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String registroGeral;
+	private String numeroCpf;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
+	private double nota1;
+	private double nota2;
+	private double nota3;
+	private double nota4;
+	
+	
 	public Aluno() {  //Cria dados na memória - sendo um padrão Java
 		
 	}
@@ -27,7 +32,152 @@ public class Aluno {
 		idade = idadePadrao;
 	}
 	
+	//Veremos os métodos setters e getters d objeto
+	//SET é para adicionar ou receber dados para os atributos.
+	//GET é para resgatar ou obter o valor do atributo.
 	
+	 public void setNome(String nome) {
+		this.nome = nome;     //this é para indicar a classe seguida do atributo. Assim é possível usar nome recebendo nome
+		
+	 }
+	 
+	 public String getNome() {
+		return nome;		 
+		
+	 }
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+	
+	//Atributo de notas
+
+	public double getNota1() {
+		return nota1;
+	}
+
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+
+	public double getNota2() {
+		return nota2;
+	}
+
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+
+	public double getNota3() {
+		return nota3;
+	}
+
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+
+	public double getNota4() {
+		return nota4;
+	}
+
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
+	}
+	
+	//Médoto que retorna a média do aluno
+	
+	public double getMediaNota() {
+		return ((nota1 + nota2 + nota3 + nota4) / 4);    
+	}
+	 
+	public boolean getAlunoAprovado() {
+		double media = this.getMediaNota();       //Método para retornar true ou false para aprovado
+		if (media >= 70) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public String getAlunoAprovado2() {
+		double media = this.getMediaNota();
+		if (media >=70) {
+			return "Aluno Aprovado!";
+		}else if (media >=50 && media<70) {
+			return "Aluno em recuperação";
+		}else {
+			return "Aluno reprovado";
+		}
+	}
+	 
 }
 
 
